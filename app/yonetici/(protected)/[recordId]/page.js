9de44c4ace3de94c2181949d982607faf56500ec
berkekarci <berkekarci@gmail.com>
2026-09-21 17:41:26 +0300

    Pass public code to PPWR file controls

--- app/yonetici/(protected)/[recordId]/page.js
+++ app/yonetici/(protected)/[recordId]/page.js
@@ -25,7 +25,7 @@ export default async function RecordPage({ params, searchParams }) {
 
       {sp?.kaydedildi && <div className="successbox">Değişiklikler kaydedildi ve kamu sayfasında doğrudan yayınlandı.</div>}
       {sp?.hata === "silme-onay" && <div className="errorbox">PPWR kaydı silinemedi: silme onayı alınamadı.</div>}
-      <RecordEditor recordData={current} recordId={recordId} action={saveRecordAction} />
+      <RecordEditor recordData={current} recordId={recordId} publicCode={record.code} action={saveRecordAction} />
     </div>
   );
 }
